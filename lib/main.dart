d429ea4507a4efe69d833ffd19f8a3b71c223874
RITika2522 <ritikachandak2502@gmail.com>
2025-03-24 12:26:55 +0530

    testing2

--- lib/main.dart
+++ lib/main.dart
@@ -64,8 +64,6 @@ class MyApp extends StatelessWidget {
         '/editques': (context) => const EditQuestion(),
         '/answers': (context) => const AllAnswerScreen(),
 
-
-
       },
     );
   }
